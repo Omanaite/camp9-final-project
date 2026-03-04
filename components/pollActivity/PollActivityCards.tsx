a21a72fa37e90e830a30af840c8109c1da19c862
Pablo Chandía <pchandia@hotmail.com>
2026-03-04 16:00:21 +0100

    h and w to Image component

--- components/pollActivity/PollActivityCards.tsx
+++ components/pollActivity/PollActivityCards.tsx
@@ -77,6 +77,8 @@ function PollActivityCards({
               src="/images/flame-288.gif"
               alt="No polls"
               className="w-[250px]"
+              width={250}
+              height={250}          
             />
             <h1 className="title-bold text-center">No polls to see.</h1>
           </div>
