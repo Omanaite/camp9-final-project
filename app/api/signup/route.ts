8688700bb6bffce382d072a87cf7bc4a97d929c6
Pablo Chandía <pchandia@hotmail.com>
2026-05-05 13:32:43 +0100

    fix: replace bcrypt imports with bcryptjs

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { SignUpSchema, signUpSchema } from '@/types/user/AuthSchemata';
 import { db } from '@/libs/db';
 import { Prisma } from '@prisma/client';
